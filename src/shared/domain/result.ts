991291547b3e807229e8cd8017e6433b983de95d
Geovanny Mendoza <me@geovannycode.com>
2025-07-19 12:00:00 -0500

    Commit retroactivo

---
+++ src/shared/domain/result.ts
@@ -0,0 +1,15 @@
+export class Result<T> {
+  constructor(
+    public readonly success: boolean,
+    public readonly error?: string,
+    public readonly value?: T
+  ) {}
+
+  static ok<T>(value?: T): Result<T> {
+    return new Result(true, undefined, value);
+  }
+
+  static fail<T>(error: string): Result<T> {
+    return new Result(false, error);
+  }
+}
